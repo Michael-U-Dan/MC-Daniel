8d88367466b9e59a2ffccede727e0da1da7cd4cf
Michael-U-Dan <michaeluzochukwua1@gmail.com>
2025-02-23 09:35:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ with open("model.pkl", "rb") as f:
   model = pickle.load(f)
 
 # give the Streamlit app page a title
-st.title("gross income Predictor")
+st.title("Quantity Predictor")
 
 # input widget for getting user value for X (feature matrix value)
 total = st.slider("Total", min_value=0, max_value=100, value=20)
 
-# After selesting gross, the user then submits the gross value
+# After selesting Quantity, the user then submits the quantity value
 if st.button("Predict"):
-  # take the price value, and format the value the right way
+  # take the quantity value, and format the value the right way
   prediction = model.predict([[total]])[0].round(2)
-  st.write("The predicted gross income is", prediction, "pounce")
+  st.write("The predicted Quantity is", prediction, "pounce")
