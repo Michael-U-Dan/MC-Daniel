ebaeceb4ef7af17a2e01241ae3b1bb8ecf74d908
Michael-U-Dan <michaeluzochukwua1@gmail.com>
2025-02-25 20:57:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,12 +11,12 @@ with open("model.pkl", "rb") as f:
 st.title("Supermarket Quantity Value Predictor" )
 
 # input widget for getting user values for X (feature matrix value)
-Unit = st.slider("Unit_price", min_value=0, max_value=100, value=20)
+Unit = st.slider("Unit price", min_value=0, max_value=100, value=20)
 Rating = st.slider("Rating", min_value=0, max_value=100, value=20)
 total = st.slider("Total", min_value=0, max_value=100, value=20)
 
 # After selesting quantity, the user then submits the price value
 if st.button("Predict"):
   # take the rating value, and format the value the right way
-  prediction = model.predict([Unit_price, Rating, Total])[0].round(3)
+  prediction = model.predict([Unit price, Rating, Total])[0].round(2)
 st.write("The predicted supermarket Quantity is", prediction)
